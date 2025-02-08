88c49aafde3126a3beb93fd912060ee6c0d99850
Jatin Chhanwal <connectwithjatin365@gmail.com>
2025-02-08 14:19:03 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,103 +1,92 @@
 var verify_existance = false;
 
 function queryBooks() {
-    
     const QUERY = (document.getElementById("query").value).toLowerCase();
     var results = document.getElementById("res");
     loader.setAttribute('style', 'display: block;');
     var query_display = document.getElementById("query-display");
 
     if (verify_existance == true) {
-            results.remove(results);
+        results.remove(results);
 
-            results = document.createElement('div');
-            results.setAttribute('id', 'res'); 
-            results.setAttribute('class', 'results'); 
+        results = document.createElement('div');
+        results.setAttribute('id', 'res'); 
+        results.setAttribute('class', 'results'); 
 
-            loader = document.createElement('div');
-            loader.setAttribute('class', 'loader');
-            loader.setAttribute('id', 'load');
-            
-            query_display = document.createElement('div');
-            query_display.setAttribute('id', 'query-display');
+        loader = document.createElement('div');
+        loader.setAttribute('class', 'loader');
+        loader.setAttribute('id', 'load');
 
-            results.appendChild(loader);
-            results.appendChild(query_display);
+        query_display = document.createElement('div');
+        query_display.setAttribute('id', 'query-display');
 
-            document.getElementById('main').appendChild(results);
-        }
+        results.appendChild(loader);
+        results.appendChild(query_display);
+
+        document.getElementById('main').appendChild(results);
+    }
 
     query_display.innerHTML = "Related results for \"" + QUERY + "\"";
 
-    const URL = "https://www.googleapis.com/books/v1/volumes?q=" + QUERY
+    const URL = "https://www.googleapis.com/books/v1/volumes?q=" + QUERY;
     
     var request = new XMLHttpRequest();
-    
-    // Open a new connection, using the GET request on the URL endpoint
     request.open('GET', URL, true);
     
     request.onload = function () {
-        // Begin accessing JSON data here
-        
         for (var i = 0; i < 10; i++) {
             var data = JSON.parse(this.response);
-            // console.log(data)
-            var authors = (data["items"][i]["volumeInfo"]["authors"]) || 'No Author Disclosed'
-            var title = (data["items"][i]["volumeInfo"]["title"]) || 'No title Disclosed'
-            var publisher = (data["items"][i]["volumeInfo"]["publisher"]) || 'No publisher Disclosed'
+            var authors = (data["items"][i]["volumeInfo"]["authors"]) || 'No Author Disclosed';
+            var title = (data["items"][i]["volumeInfo"]["title"]) || 'No title Disclosed';
+            var publisher = (data["items"][i]["volumeInfo"]["publisher"]) || 'No publisher Disclosed';
             try {
-            var thumbnail = data["items"][i]["volumeInfo"]["imageLinks"]["thumbnail"] 
+                var thumbnail = data["items"][i]["volumeInfo"]["imageLinks"]["thumbnail"];
+            } catch (err) {
+                var thumbnail = 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Georgia_404.svg/1125px-Georgia_404.svg.png';
             }
-            catch (err) {
-                var thumbnail = 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Georgia_404.svg/1125px-Georgia_404.svg.png'
-            }
 
-        var info = (data["items"][i]["volumeInfo"]["infoLink"]) || 'No info Disclosed'
-            
-            
-        card = document.createElement('div');
-        card.setAttribute('class', 'card col-md-6');
-        card.setAttribute('style', 'max-width: 32rem;');
-        card.setAttribute('id', 'results');
+            var info = (data["items"][i]["volumeInfo"]["infoLink"]) || 'No info Disclosed';
 
+            var card = document.createElement('div');
+            card.setAttribute('class', 'custom-card');
+            card.setAttribute('id', 'results');
 
-        const logo = document.createElement('img');
-        logo.src = thumbnail;
-        logo.className = "card-img-top"
+            const logo = document.createElement('img');
+            logo.src = thumbnail;
+            logo.className = "custom-card-img";
     
-        card.appendChild(logo);
+            card.appendChild(logo);
         
-        const card_body = document.createElement('div');
-        card_body.setAttribute('class', 'card-body');
+            const card_body = document.createElement('div');
+            card_body.setAttribute('class', 'custom-card-body');
         
-        const card_title = document.createElement('h5');
-        card_title.setAttribute('class', 'card-title');
-        card_title.innerHTML = title;
+            const card_title = document.createElement('h5');
+            card_title.setAttribute('class', 'custom-card-title');
+            card_title.innerHTML = title;
         
-        card_body.appendChild(card_title);
+            card_body.appendChild(card_title);
         
-        const card_text = document.createElement('p');
-        card_text.setAttribute('class', 'card-text');
-        card_text.innerHTML = "By: " + authors + "<br>Published By: " + publisher;
+            const card_text = document.createElement('p');
+            card_text.setAttribute('class', 'custom-card-text');
+            card_text.innerHTML = "By: " + authors + "<br>Published By: " + publisher;
         
-        card_body.appendChild(card_text);
+            card_body.appendChild(card_text);
         
-        const button = document.createElement('a');
-        button.setAttribute('class', 'btn btn-primary btn-md');
-        button.setAttribute('href', info);
-        button.innerHTML = "See this Book"
+            const button = document.createElement('a');
+            button.setAttribute('class', 'custom-button');
+            button.setAttribute('href', info);
+            button.innerHTML = "See this Book";
         
-        card_body.appendChild(button);
+            card_body.appendChild(button);
 
-        card.appendChild(card_body);
+            card.appendChild(card_body);
         
-        results.appendChild(card); }
-    } 
+            results.appendChild(card);
+        }
+    };
     
     verify_existance = true;
-    // Send request
-    request.send()
-    document.getElementById('query').value = ''
-    setTimeout("loader.setAttribute('style', 'display: none;')", 1500); 
-    
+    request.send();
+    document.getElementById('query').value = '';
+    setTimeout(() => loader.setAttribute('style', 'display: none;'), 1500);
 }
